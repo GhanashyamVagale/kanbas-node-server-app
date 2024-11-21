073a13a1357a882857cfcfbd32b44c7d0817b91a
Ghanashyam Vagale <73483236+GhanashyamVagale@users.noreply.github.com>
2024-11-21 01:53:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "https://gvs-kanbas-react-web-app.netlify.app" || "https://a5--gvs-kanbas-react-web-app.netlify.app",
+    origin: process.env.NETLIFY_URL || "https://a5--gvs-kanbas-react-web-app.netlify.app",
   })
 );
 const sessionOptions = {
